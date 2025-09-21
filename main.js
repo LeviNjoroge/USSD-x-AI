7545d3d211d3c5d165b42e0b850b443e5d78453f
Levi Njoroge <Levynjoroge1@gmail.com>
2025-09-21 22:57:45 +0300

    Creating a way to use AI using USSD

--- main.js
+++ main.js
@@ -2,4 +2,5 @@
 Create an AI sending responces to a node 
 Create a USSD on the node that sends and receives data between the user and the AI
 max of 5 req, res
+see how I can launch this on the mainnet
 */
